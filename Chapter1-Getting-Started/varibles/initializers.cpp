b481d72e48b809d768a024ce14b0e6bce493b50b
monpeco <monpeco@gmail.com>
2018-03-01 15:29:42 +0000

    Add variable's initialization with applyDiscount()

--- Chapter1-Getting-Started/varibles/initializers.cpp
+++ Chapter1-Getting-Started/varibles/initializers.cpp
@@ -1,20 +1,31 @@
 #include <iostream>
 
+double applyDiscount(double price, double discount);
+  
 int main(){
   
   // Initialization happens when a variable is give a value when it is created.
   // Using a initialized variable to initialize another
   double price = 109.99, discount = price * 0.15;
   
+  //ok: call applyDiscount and use the return value to initialize salePrice
+  double salePrice = applyDiscount(price, discount);
+  
   std::cout << "price: " << price
             << ", discount: " << discount << std::endl;
+  
+  std::cout << "salePrice: " << salePrice << std::endl;
   
   return 0;
 }
 
+double applyDiscount(double price, double discount){
+  return price - discount;
+}
 /*
 Output:
 
 price: 109.99, discount: 16.4985
+salePrice: 93.4915
 
 */
