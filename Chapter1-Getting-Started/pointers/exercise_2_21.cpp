d4adae05a7412626b1ad4b8a6f760d4ce029e4cc
monpeco <monpeco@gmail.com>
2018-03-06 18:16:28 +0000

    Add cannot convert int to long int*

--- Chapter1-Getting-Started/pointers/exercise_2_21.cpp
+++ Chapter1-Getting-Started/pointers/exercise_2_21.cpp
@@ -2,10 +2,17 @@
 
 int main(){
 
-  int i = 0;
+  int i = 1;
   //double* dp = &i;      //exercise_2_21.cpp:6:17: error: cannot convert ‘int*’ to ‘double*’ in initialization
   //int *ip = i;          //exercise_2_21.cpp:7:13: error: invalid conversion from ‘int’ to ‘int*’ [-fpermissive]
   int *p = &i;
+  
+  if (p) std::cout << "p: " << p << std::endl;
+  if (*p) std::cout << "*p: " << *p << std::endl;
+  
+  int i2 = 42; 
+  void *p2 = &i2; 
+  //long *lp2 = &i2;      //exercise_2_21.cpp:15:16: error: cannot convert ‘int*’ to ‘long int*’ in initialization
   
   return 0;
 }
