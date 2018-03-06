a1286c61cbc2b318daf11d8fd1fdaebb2e18a388
monpeco <monpeco@gmail.com>
2018-03-06 19:20:22 +0000

    Create exercise 2.25

---
+++ Chapter1-Getting-Started/pointers/exercise_2_25.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+  
+  //int* ip, &r = ip;     //exercise_2_25.cpp:5:17: error: invalid initialization of reference of type ‘int&’ from expression of type ‘int*’
+  int i, *ip = 0;         //initializes ip from the literal constant 0 
+  int* p, ip2;
+
+  return 0;
+}
