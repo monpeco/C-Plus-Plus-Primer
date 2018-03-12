4125d9a6d7c79b6f30824bf6e869261eca157c32
monpeco <monpeco@gmail.com>
2018-03-12 21:24:05 +0000

    Add constexpr imposes top-level const

--- Chapter1-Getting-Started/pointers/constexpr.cpp
+++ Chapter1-Getting-Started/pointers/constexpr.cpp
@@ -11,6 +11,10 @@ int main(){
   constexpr int limit = mf + 1; // mf + 1 is a constant expression
   constexpr int sz = size(); // ok only if size is a constexpr function
   
+
+  const int *p = nullptr;      // p is a pointer to a const int
+  constexpr int *q = nullptr;  // q is a const pointer to int. constexpr imposes a top-level const on the objets it defines
+  
   return 0;
 }
 
