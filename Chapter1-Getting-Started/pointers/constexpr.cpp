902a75d05330b02adfcdb83b4c9445e58aa65b31
monpeco <monpeco@gmail.com>
2018-03-12 21:19:37 +0000

    Create constexpr example

---
+++ Chapter1-Getting-Started/pointers/constexpr.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+constexpr int size(){
+  
+  return 7;
+}
+
+int main(){
+  
+  constexpr int mf = 20; // 20 is a constant expression
+  constexpr int limit = mf + 1; // mf + 1 is a constant expression
+  constexpr int sz = size(); // ok only if size is a constexpr function
+  
+  return 0;
+}
+
