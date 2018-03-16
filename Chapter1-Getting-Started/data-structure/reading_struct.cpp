cb56929899c822717f0e3061f2bf302f79060ef3
monpeco <monpeco@gmail.com>
2018-03-16 18:57:06 +0000

    Add reading the second transaction

--- Chapter1-Getting-Started/data-structure/reading_struct.cpp
+++ Chapter1-Getting-Started/data-structure/reading_struct.cpp
@@ -9,15 +9,27 @@ struct sales_data{
 
 int main(){
   
-  sales_data s1;
+  sales_data s1, s2;
   
   double price{0};
-  std::cout << "Int. values: " << std::endl;
-  std::cin >> s1.bookNo >> s1.units_sold >> s1.revenue;
+  std::cout << "read the first transaction: " << std::endl;
+  std::cin >> s1.bookNo >> s1.units_sold >> price;
+  
+  s1.revenue = s1.units_sold * price;
   
   std::cout << "bookNo: " << s1.bookNo 
             <<  ", units_sold: " << s1.units_sold 
             <<  ", revenue: " << s1.revenue << std::endl;
+
+
+  std::cout << "read the second transaction: " << std::endl;
+  std::cin >> s2.bookNo >> s2.units_sold >> price;
+  
+  s2.revenue = s2.units_sold * price;
+  
+  std::cout << "bookNo: " << s2.bookNo 
+            <<  ", units_sold: " << s2.units_sold 
+            <<  ", revenue: " << s2.revenue << std::endl;
 
   return 0;
 }
