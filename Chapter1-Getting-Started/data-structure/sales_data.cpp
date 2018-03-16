1795c887ce1f66a08344b3b46aa555370b9b64e4
monpeco <monpeco@gmail.com>
2018-03-16 15:07:05 +0000

    Change in-class initializer to curly-braces form

--- Chapter1-Getting-Started/data-structure/sales_data.cpp
+++ Chapter1-Getting-Started/data-structure/sales_data.cpp
@@ -3,13 +3,15 @@
 
 struct Sales_data{
   std::string bookNo;
-  unsigned units_sold = 0;
-  double revenue = 0.0;
+  unsigned units_sold{2};
+  double revenue{4.0};
 };
 
 int main(){
   
   Sales_data s1;
+  std::cout << s1.bookNo << ' ' << s1.units_sold << ' ' << s1.revenue << std::endl;
+  
   s1.bookNo = "ISBN 45787";
   s1.units_sold = 2;
   s1.revenue = 14.75;
