7899dffac9cd9068cc61619a0f39838c036064cf
monpeco <monpeco@gmail.com>
2018-03-20 17:26:43 +0000

    Add check for SALES_DATA_H header guard

--- Chapter1-Getting-Started/header-guard/Sales_data2.h
+++ Chapter1-Getting-Started/header-guard/Sales_data2.h
@@ -1,5 +1,21 @@
-
+#ifndef SALES_DATA_H
+#define SALES_DATA_H
 #include <string>
 #include "Sales_data.h"
 
+
+
+#endif
+
+#ifndef SALES_DATA_H2
+#define SALES_DATA_H2
+
+struct Sales_data2{
+  unsigned amount = 0;
+  double total = 0.0;
+};
+
+
+
+#endif
 
