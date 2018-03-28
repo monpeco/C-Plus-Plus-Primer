3724f7a6ca68b7542c6f184423ed2babed924d5a
monpeco <monpeco@gmail.com>
2018-03-28 02:10:04 +0000

    Add read 2 strings and unknown number of strings

--- Chapter3-Strings-Vectors-and-Arrays/string/read_write.cpp
+++ Chapter3-Strings-Vectors-and-Arrays/string/read_write.cpp
@@ -8,5 +8,21 @@ int main(){
   std::cin >> s;
   
   std::cout << "s: " << s << std::endl;
+  
+  
+  
+  std::string s1, s2;
+  
+  std::cin >> s1 >> s2;
+  
+  std::cout << "s1: " << s1 << ", s2: " << s2 << std::endl;
+  
+  
+  std::string word;
+  
+  while(std::cin >> word){
+    std::cout << "word: " << word << std::endl; 
+  }
+  
   return 0;
 }
