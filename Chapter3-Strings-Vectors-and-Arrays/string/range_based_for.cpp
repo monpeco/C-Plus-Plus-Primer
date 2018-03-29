8b832122f1ce26d9ddffe6c5f7db6fd6871c9034
monpeco <monpeco@gmail.com>
2018-03-29 02:06:21 +0000

    Add counting the puntuation character

--- Chapter3-Strings-Vectors-and-Arrays/string/range_based_for.cpp
+++ Chapter3-Strings-Vectors-and-Arrays/string/range_based_for.cpp
@@ -8,6 +8,16 @@ int main(){
   for(auto c : str)  // C++11
     std::cout << c << std::endl;
   
+  // count the puntuation characters
+  std::string s("Hello world!!!");
+  
+  decltype(s.size()) punct_cnt = 0;
+  
+  for(auto c : s)
+    if( ispunct(c) )
+      ++punct_cnt;
+  
+  std::cout << punct_cnt << " punctuation characters in " << s << std::endl;
   
   return 0;
 }
