47ae01f015ced7fcf6a73d5e7ffd8224c3cc90c3
monpeco <monpeco@gmail.com>
2018-04-01 01:32:05 +0000

    Create vector example

---
+++ Chapter3-Strings-Vectors-and-Arrays/vector/vector.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <string>
+
+using std::vector;
+using std::string;
+
+int main(){
+  
+  vector<int> ivec;
+  
+  vector<vector<string>> file; // in c++98 vector<vector<string> > we must supply an extra space between the close angle bracket
+  
+  return 0;
+}
