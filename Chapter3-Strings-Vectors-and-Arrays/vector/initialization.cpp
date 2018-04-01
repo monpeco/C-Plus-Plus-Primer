cd27c8744eda08740cbc9a8b0cad70bd6a7aa2b9
monpeco <monpeco@gmail.com>
2018-04-01 03:11:38 +0000

    Add list initializer of element count example

--- Chapter3-Strings-Vectors-and-Arrays/vector/initialization.cpp
+++ Chapter3-Strings-Vectors-and-Arrays/vector/initialization.cpp
@@ -27,5 +27,16 @@ int main(){
   vector<T> v55 = {a,b,c};        // c++11 v4 has n copies of the value-initialized value
 
 
+  // List Initializer or Element Count?
+  
+  vector<int> v10(10);    // v1 has ten elements with value 0
+  
+  vector<int> v20{10};    // v2 has one element with value 10
+  
+  vector<int> v30(10, 1); // v3 has ten elements with value 1
+  
+  vector<int> v40{10, 1}; // v4 has two elements with values 10 and 1
+
+
   return 0;
 }
