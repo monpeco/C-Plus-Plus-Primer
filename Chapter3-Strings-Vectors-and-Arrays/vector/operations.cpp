d849d24a6a7c3db7dc40350d8e50ce95ad56df39
monpeco <monpeco@gmail.com>
2018-04-03 01:02:09 +0000

    Add (rest of) operations on vector

--- Chapter3-Strings-Vectors-and-Arrays/vector/operations.cpp
+++ Chapter3-Strings-Vectors-and-Arrays/vector/operations.cpp
@@ -5,8 +5,9 @@ using namespace std;
 
 int main(){
   
-  vector<int> v;
+  vector<int> v, v1, v2;
   int i = 0;
+  int n = 0;
   
   cout << v.empty() <<endl;     // Returns true if v is empty
 
@@ -14,6 +15,24 @@ int main(){
 
   v.push_back(i);               // Adds an element with value i at the end
 
+  cout << v[n] <<endl;          // Returns a reference to the element at position n in v
   
+  v1 = v2;                      // Replace the elements in v1 with a copy of the elements in v2
+
+  v1 = {n, i};                  // C++11 - Replace the elements in v1 with a copy of the elements in the comma-separeted list
+  
+  cout << (v1 == v2) <<endl;      // v1 and v2 are equal if they have the same number of elements and 
+                                // each element in v1 is equal to the corresponding element in v2
+
+  cout << (v1 != v2) <<endl; 
+
+  cout << (v1 < v2) <<endl; 
+
+  cout << (v1 <= v2) <<endl; 
+
+  cout << (v1 > v2) <<endl; 
+
+  cout << (v1 >= v2) <<endl; 
+
   return 0;
 }
