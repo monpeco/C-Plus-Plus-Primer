85965a09b1622b6fda67f293fbf9b8b81d6a347c
monpeco <monpeco@gmail.com>
2018-04-03 21:28:06 +0000

    Create capitalize first word example with iterators

---
+++ Chapter3-Strings-Vectors-and-Arrays/iterator/capitalize_words.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+  
+  string s("some string");
+  
+  cout << s << endl;          // some string
+  
+  for(auto it = s.begin(); it != s.end() && !isspace(*it); ++it){
+    *it = toupper(*it);
+  }
+  
+  cout << s << endl;          // SOME string
+  
+  return 0;
+}
