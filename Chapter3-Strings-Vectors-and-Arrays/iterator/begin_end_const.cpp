ce4d4840a7ea2c553bafc9dd2f0c2c65b7a14e5d
monpeco <monpeco@gmail.com>
2018-04-05 18:38:25 +0000

    Create example with diff between iterators on vector and const vector

---
+++ Chapter3-Strings-Vectors-and-Arrays/iterator/begin_end_const.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+  
+  vector<int> v;
+  const vector<int> cv;
+  
+  auto it1 = v.begin();     // it1 has type vector<int>::iterator
+  auto it2 = cv.begin();    // it2 has type vector<int>::const_iterator
+  
+  return 0;
+}
