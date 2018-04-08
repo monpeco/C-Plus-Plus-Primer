c885440f3e341f991472bc7991d4063356d5716f
monpeco <monpeco@gmail.com>
2018-04-08 23:48:05 +0000

    Add accesing elements in multidimensional arrays

--- Chapter3-Strings-Vectors-and-Arrays/array/initialize_multidimentional.cpp
+++ Chapter3-Strings-Vectors-and-Arrays/array/initialize_multidimentional.cpp
@@ -21,6 +21,11 @@ int main(){
   
   int ia4[3][4] = {0,3,6,9};
 
+  // assigns the first element of arr to the last element in the last row of ia
+  int arr[10][20][30] = {0}; // initialize all elements to 0
+  ia4[2][3] = arr[0][0][0];
+  int (&row)[4] = ia4[1]; // binds row to the second four-element array in ia
+
   return 0;
 }
 
