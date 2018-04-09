91ff724917ae64f4010243bafc3afc82ca1f8c0c
monpeco <monpeco@gmail.com>
2018-04-09 01:58:18 +0000

    Add ranged for loops

---
+++ Chapter3-Strings-Vectors-and-Arrays/array/nested_ranged_for.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+int main(){
+  constexpr size_t rowCnt = 3, colCnt = 4;
+  int ia[rowCnt][colCnt];   // 12 uninitialized elements
+  // for each row
+  for (size_t i = 0; i != rowCnt; ++i) {
+      // for each column within the row
+      for (size_t j = 0; j != colCnt; ++j) {
+          // assign the element's positional index as its value
+          ia[i][j] = i * colCnt + j;
+      }
+  }
+  
+  size_t r cnt = 0;
+  for(auto &row : ia)
+    for(auto &col : row){
+      col = cnt;
+      ++cnt;
+    }
+  
+  return 0;
+}
+
+/*
+Output:
+
+*/
