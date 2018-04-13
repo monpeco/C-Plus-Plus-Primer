d0178646fc184e74d708778fad4fb8fb78491f39
monpeco <monpeco@gmail.com>
2018-04-13 20:31:51 +0000

    Add use of 'Type Aliases' (IMPORTANT)

--- Chapter3-Strings-Vectors-and-Arrays/array/multi_dim_array_pointer.cpp
+++ Chapter3-Strings-Vectors-and-Arrays/array/multi_dim_array_pointer.cpp
@@ -17,7 +17,8 @@ int main(){
   * With the advent of c++11, we can often avoid having to write the type of a 
   * pointer into an array by using auto of decltype
   */
-
+  
+  cout << "use auto in for" << endl;
   int count = 10;
   for(auto p = ia; p != ia + 3; ++p){
     for(auto q = *p; q != *p + 4; ++q){
@@ -39,7 +40,7 @@ int main(){
   */
   
   // p points to the first in ia
-  cout << endl;
+  cout << endl << "use begin and end functions" << endl;
   for(auto p1 = begin(ia); p1 != end(ia); ++p1){
     for(auto q1 = begin(*p1); q1 != end(*p1); ++q1){
       cout << *q1 << ' ';
@@ -47,5 +48,40 @@ int main(){
     cout << endl;
   }
   
+  /*
+  * type aliases simplify pointers to multidimensional arrays
+  */
+  
+  cout << endl << "use type aliases" << endl;
+  
+  using int_array = int[4];
+  
+  typedef int int_array[4];
+  
+  for(int_array *p2 = ia; p2 != ia + 3; ++p2){
+    for(int *q2 = *p2; q2 != *p2 + 4 ; ++q2){
+      cout << *q2 << ' ';
+    }
+    cout << endl;
+  }
+  
   return 0;
 }
+
+/* Output:
+
+use auto in for
+10 11 12 13 
+14 15 16 17 
+18 19 20 21 
+
+use begin and end functions
+10 11 12 13 
+14 15 16 17 
+18 19 20 21 
+
+use type aliases
+10 11 12 13 
+14 15 16 17 
+18 19 20 21
+*/
