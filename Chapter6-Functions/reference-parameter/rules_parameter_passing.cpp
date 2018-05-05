0e01111078f2a7e04fcbecd4cff1cd140a21ab25
monpeco <monpeco@gmail.com>
2018-05-05 23:10:46 +0000

    Add pointer as parameter

--- Chapter6-Functions/reference-parameter/rules_parameter_passing.cpp
+++ Chapter6-Functions/reference-parameter/rules_parameter_passing.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 void reset(int &i);
+void reset(int *i);
 
 int main(){
   
@@ -9,16 +10,25 @@ int main(){
   std::string::size_type ctr = 0;
 
   std::cout << "before reset - v: " << i << std::endl;
-
   reset(i);
-  
   std::cout << "after reset - v: " << i << std::endl;
+
+  // reset(ci);    // invalid initialization of reference of type ‘int&’ from expression of type ‘const int’
+
+  std::cout << "before reset - v: " << i << std::endl;
+  reset(&i);
+  std::cout << "after reset - v: " << i << std::endl;
+
 
   return 0;
 }
 
 void reset(int &i){
   i = 6;
+}
+
+void reset(int *i){
+  *i = 7;
 }
 
 /*
