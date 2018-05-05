3a4023008264a1513f450d775e2b9428683d0dfc
monpeco <monpeco@gmail.com>
2018-05-05 23:14:10 +0000

    Add invalid conversion from const int* to int*

--- Chapter6-Functions/reference-parameter/rules_parameter_passing.cpp
+++ Chapter6-Functions/reference-parameter/rules_parameter_passing.cpp
@@ -14,10 +14,14 @@ int main(){
   std::cout << "after reset - v: " << i << std::endl;
 
   // reset(ci);    // invalid initialization of reference of type ‘int&’ from expression of type ‘const int’
+                   // error: can't bind a plain reference to the const object ci
 
   std::cout << "before reset - v: " << i << std::endl;
   reset(&i);
   std::cout << "after reset - v: " << i << std::endl;
+
+  // reset(&ci);    // error: invalid conversion from ‘const int*’ to ‘int*’ [-fpermissive]
+                 // error: can't initialize an int* from a pointer to a const int object
 
 
   return 0;
