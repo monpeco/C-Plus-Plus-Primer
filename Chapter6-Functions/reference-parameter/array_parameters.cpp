1fbf744be34e1b936bbe162cc9b2f42ef5ca9de2
monpeco <monpeco@gmail.com>
2018-05-08 21:07:39 +0000

    Add array operation as pointers

--- Chapter6-Functions/reference-parameter/array_parameters.cpp
+++ Chapter6-Functions/reference-parameter/array_parameters.cpp
@@ -5,7 +5,7 @@ void print1(const int*);
 
 int main(){
   
-  int i[] = {1,2,3,4,5,6,7,8,9,0};
+  int i[] = {10,22,30,44,50,66,70,88,99,100};
   
   print1(i);
   
@@ -14,6 +14,8 @@ int main(){
 
 void print1(const int* i){
   std::cout << *i << std::endl;
+  std::cout << *i+5 << std::endl;
+  std::cout << *(i+5) << std::endl;
 }
 
 
@@ -21,5 +23,8 @@ void print1(const int* i){
 /*
 Output:
 
-1
+10
+15
+66
+
 */
