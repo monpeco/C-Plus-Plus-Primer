e46091e47c1a8a9820f930c1f5b1120784dfb1ff
monpeco <monpeco@gmail.com>
2018-05-15 21:37:09 +0000

    Create argument passing example

---
+++ Chapter6-Functions/argument-passing/argument_passing.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+
+int main(int argc, char *argv[]){
+  
+  std::cout << argv[0] << std::endl;
+  std::cout << argv[1] << std::endl;
+
+  return 0;
+}
+
+
+
+/*
+Output:
+
+./a.out
+84
+
+*/
