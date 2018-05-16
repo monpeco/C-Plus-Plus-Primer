16df3905caa2b099fed7ca0523f9e94bb7daa5c1
monpeco <monpeco@gmail.com>
2018-05-16 22:23:13 +0000

    Add another parameter to function with initializer list

--- Chapter6-Functions/argument-passing/initializer_list.cpp
+++ Chapter6-Functions/argument-passing/initializer_list.cpp
@@ -3,6 +3,8 @@
 
 void error_msg(std::initializer_list<std::string> il);
 
+void error_msg(int type, std::initializer_list<std::string> il);
+
 int main(int argc, char *argv[]){
   std::initializer_list<std::string> ls;    // initializer_list of strings
   std::initializer_list<int> li;            // initializer_list of ints
@@ -10,6 +12,11 @@ int main(int argc, char *argv[]){
   error_msg({"functionX", "expected", "actual"});
 
   error_msg({"functionX", "expected", "actual", "wind", "high"});
+
+  // sending another parameter
+  error_msg(3, {"functionX", "expected", "actual", "wind", "high"});
+  error_msg(7, {"functionX", "ex"});
+
 
   return 0;
 }
@@ -20,6 +27,16 @@ void error_msg(std::initializer_list<std::string> il){
   }
   
   std::cout << std::endl;
+  
+}
+
+
+void error_msg(int type, std::initializer_list<std::string> il){
+  for (auto beg = il.begin(); beg != il.end(); ++beg){
+    std::cout << *beg << std::endl;
+  }
+  
+  std::cout << "type:" << type << std::endl << std::endl;
   
 }
 
@@ -35,5 +52,16 @@ expected
 actual
 wind
 high
+
+functionX
+expected
+actual
+wind
+high
+type:3
+
+functionX
+ex
+type:7
 
 */
