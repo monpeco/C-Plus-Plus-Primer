770cfbfcc687c9df48362111b790236f5e31a453
monpeco <monpeco@gmail.com>
2018-07-15 22:38:37 +0000

    Add call to fact function

---
+++ Chapter6-Functions/compilation-linking/factMain.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Chapter6.h"
+
+using namespace std;
+
+int main()
+{
+    int j = fact(5);  // j equals 120, i.e., the result of fact(5)
+    cout << "5! is " << j << endl;
+    return 0;
+}
+
