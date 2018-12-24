892de1803797265928013495e72b2a95a46c1562
monpeco <monpeco@gmail.com>
2018-12-24 17:11:51 +0000

    Add lookup process to find type names

---
+++ Chapter7-Classes/7.4-class-scope/lookup_member_declarations.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+typedef double Money;
+string bal;
+class Account{
+  public:
+    //  it will look for a declaration of Money in the Account class. The compiler considers only declarations
+    // inside Account that appear before the use of Money. Because no matching member
+    // is found, the compiler then looks for a declaration in the enclosing scope(s) and find typedef double Money;
+    
+    Money balance(){ return bal; };  // On the other hand, the function body of balance is processed only after the entire class
+                                     // is seen. Thus, the return inside that function returns the member named bal, not
+                                     // the string from the outer scope
+    
+  private:
+    Money bal;
+};
+
+
+int main(){
+
+  
+  return 0;
+}
