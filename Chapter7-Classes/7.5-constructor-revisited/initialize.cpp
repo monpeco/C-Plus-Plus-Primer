98dc82c364d2dc79f4e460b4be439e5623598be9
monpeco <monpeco@gmail.com>
2018-12-25 17:17:42 +0000

    Add constructor initializers are sometimes required

---
+++ Chapter7-Classes/7.5-constructor-revisited/initialize.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class ConstRef{
+  public:
+    ConstRef(int ii);
+    
+  private:
+    int i;
+    const int ci;
+    int &ri;
+};
+
+// error: ci and ri must be initialized
+ConstRef::ConstRef(int ii){
+  i = ii;    // ok
+  ci = ii;   // error: cannot assign to a const
+  ri = i;    // error: ri was never initialized
+}
+
+
+int main(){
+  string foo = "Hello world!";  // define and initialize
+  string bar;                   // default initialized the empty string
+  bar = "Hello world!";         // assing a new value to bar
+  return 0;
+}
