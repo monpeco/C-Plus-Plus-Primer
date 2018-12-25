231bddb0157cf2e07c67764c0eaa6c81e1aa90aa
monpeco <monpeco@gmail.com>
2018-12-25 18:41:12 +0000

    Add constructor initializer - change assigment to initialize

--- Chapter7-Classes/7.5-constructor-revisited/initialize.cpp
+++ Chapter7-Classes/7.5-constructor-revisited/initialize.cpp
@@ -14,12 +14,13 @@ class ConstRef{
 };
 
 // error: ci and ri must be initialized
-ConstRef::ConstRef(int ii){
-  i = ii;    // ok
-  ci = ii;   // error: cannot assign to a const
-  ri = i;    // error: ri was never initialized
-}
+// ConstRef::ConstRef(int ii){
+//   i = ii;    // ok
+//   ci = ii;   // error: cannot assign to a const
+//   ri = i;    // error: ri was never initialized
+// }
 
+ConstRef::ConstRef(int ii) : i(ii), ci(ii), ri(i) {}
 
 int main(){
   string foo = "Hello world!";  // define and initialize
