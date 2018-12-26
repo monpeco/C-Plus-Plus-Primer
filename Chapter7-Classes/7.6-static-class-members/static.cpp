da0cf6553f46098571f7be3cbbf68d6bf6be2d73
monpeco <monpeco@gmail.com>
2018-12-26 23:06:46 +0000

    Add using a class static member

--- Chapter7-Classes/7.6-static-class-members/static.cpp
+++ Chapter7-Classes/7.6-static-class-members/static.cpp
@@ -15,6 +15,15 @@ private:
 };
 
 int main(){
+  double r;
+  r = Account::rate();  // access a static memeber using the scope operator
+  
+  Account ac1;
+  Account *ac2 = &ac1;
+  
+  //equivalent ways to call the static member rate function
+  r = ac1.rate();   // through an Account object or reference
+  r = ac2->rate();  // throught a pointer to an Account object
   
   
   return 0;
