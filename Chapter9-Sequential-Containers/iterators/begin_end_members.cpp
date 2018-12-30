7cd1118f4a71daa9a3b198de9973fca987838991
monpeco <monpeco@gmail.com>
2018-12-30 01:14:48 +0000

    Add begin and end members and use of auto

---
+++ Chapter9-Sequential-Containers/iterators/begin_end_members.cpp
@@ -0,0 +1,23 @@
+#include <list>
+#include <string>
+
+using namespace std;
+
+int main(){
+  
+  list<string> a = {"Muy", "Alto", "otro"};
+  auto it1 = a.begin();      // list<string>::iterator
+  auto it2 = a.rbegin();     // list<string>::reverse_iterator
+  auto it3 = a.cbegin();     // list<string>::const_iterator
+  auto it4 = a.crbegin();    // list<string>::const_reverse_iterator
+  
+  // type is explicitly specified
+  list<string>::iterator it5 = a.begin();
+  list<string>::const_iterator it6 = a.begin();
+  
+  // iterator or const iterator depending on a's type of a 
+  auto it7 = a.begin();    // const_iterator only if a is const
+  auto it8 = a.cbegin();   // const_iterator
+  
+  return 0;
+}
