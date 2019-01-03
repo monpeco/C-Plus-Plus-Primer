afcd0d08ff73d8afa26a8dd096cfa98a078f8518
monpeco <monpeco@gmail.com>
2019-01-03 22:17:41 +0000

    Add subscripting and safe random access

---
+++ Chapter9-Sequential-Containers/9.3-Sequential-Container-Operations/subscripting_and_safe_random_access.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+  
+  vector<string> svec;    // empty vector
+  cout << svec[0];        // run-time error: there are no elements in svec
+  cout << svec.at(0);     // throws an `out_of_range` exception
+
+  return 0;
+}
