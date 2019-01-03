9b70297d3e4e808b762bda2e37fb540eb163d876
monpeco <monpeco@gmail.com>
2019-01-03 22:06:08 +0000

    Add check that there are elements before dereferencing an iterator

--- Chapter9-Sequential-Containers/9.3-Sequential-Container-Operations/access_elements.cpp
+++ Chapter9-Sequential-Containers/9.3-Sequential-Container-Operations/access_elements.cpp
@@ -19,7 +19,25 @@ int main(){
   e = c.at(n);             // Returns a reference to tha element indexed by n. If the index is out of range, throws an `out_of_range` exception
   cout << e << endl;
   
+  // each sequential container has a `front` member
+  // and all except `forward_list` also have a `back` member.
   
+  
+  // check that there are elements before dereferencing an interator or calling `front` or `back`
+  if (!c.empty()){
+    // val and val2 are copies of the value of first element in c
+    auto val = *c.begin(), val2 = c.front();
+    
+    // val3 and val4 are copies of the last element in c
+    auto last = c.end();
+    auto val3 = *(--last);
+    cout << "val3:" << val3 << endl;
+
+    auto val4 = c.back();
+    cout << "val4:" << val4 << endl;
+
+  }
+
   return 0;
 }
 
@@ -30,5 +48,7 @@ Output:
 11
 10
 10
+val3:5
+val4:5
 
 */
