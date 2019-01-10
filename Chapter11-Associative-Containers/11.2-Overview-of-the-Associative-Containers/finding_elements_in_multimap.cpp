87b1c6f7edb634e6e0d3a5719b6b8284b8cedc68
monpeco <monpeco@gmail.com>
2019-01-10 22:08:57 +0000

    Add finding elements in a multimap - basic example

---
+++ Chapter11-Associative-Containers/11.2-Overview-of-the-Associative-Containers/finding_elements_in_multimap.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main(){
+  
+  // Finding an element in a multiple keys container is more complicated.
+  // There may be many elements with the given key. When a `multimap` or a `multiset`
+  // has multiple elements of a given key, those elements will be adjecent w
+  // ithin the container:
+
+  map<string, string> authors = {{"Joyce", "James"}, {"Austen", "Jean"}, {"Dickens", "Charles"}};
+  string search_item("Austen");  // author we'll be looking for
+  
+  auto entries = authors.count(search_item); // number of elements
+  auto iter = authors.find(search_item); // first entry for this author
+  
+  // loop through the number of entries there are for this author
+  while(entries){
+    cout << iter->second << endl;   // print each title
+    ++iter;    // advance to the next title
+    --entries;  // keep track of how many we've printed
+  }
+  
+  return 0;
+}
