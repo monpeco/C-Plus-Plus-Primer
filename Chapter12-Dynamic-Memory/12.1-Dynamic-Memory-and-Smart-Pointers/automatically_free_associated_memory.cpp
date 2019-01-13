a16bb2d766c3cf0c5e94491b6ddc6a8468aa6f24
monpeco <monpeco@gmail.com>
2019-01-13 16:08:52 +0000

    Add returning the shared_ptr in a function

--- Chapter12-Dynamic-Memory/12.1-Dynamic-Memory-and-Smart-Pointers/automatically_free_associated_memory.cpp
+++ Chapter12-Dynamic-Memory/12.1-Dynamic-Memory-and-Smart-Pointers/automatically_free_associated_memory.cpp
@@ -13,14 +13,32 @@ private:
   int bar;
 };
 
+// factory returns a shared_ptr pointing to a dynamucally allcoated object
 shared_ptr<Foo> factory(){
-  return make_shared<Foo>(4);
+  return make_shared<Foo>(4);   // shared_ptr will take care of deleting this memory
 }
 
-int main(){
-  auto p = factory();
+// Because factory returns a shared_ptr, we can be sure that the object
+// allocated by factory will be freed when appropriate. For example, the
+// following function stores the shared_ptr returned by factory in a local
+// variable:
+void use_factory(){
+  shared_ptr<Foo> p = factory();
   p->print_bar();
-  p = nullptr;
+  // use p
+}       // p goes out of scope; the memory to which p points is automatically freed
+
+// The memory will not be freed if there is any other shared_ptr pointing to it
+shared_ptr<Foo> return_factory(){
+  shared_ptr<Foo> p = factory();
+  p->print_bar();
+  return p;   // reference count is incremented when we return p
+}   // p goes out of scope; the memory to which p points is not freed
+
+int main(){
+  
+  use_factory();
+  shared_ptr<Foo> p = return_factory();
   
   return 0;
 }
