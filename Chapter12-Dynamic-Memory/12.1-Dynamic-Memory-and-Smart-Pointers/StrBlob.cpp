816ace2c52728d7d1ec84fe72b7e5c4b4b909bc8
monpeco <monpeco@gmail.com>
2019-01-14 22:06:10 +0000

    Add creation of StrBlolb class

---
+++ Chapter12-Dynamic-Memory/12.1-Dynamic-Memory-and-Smart-Pointers/StrBlob.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <memory>
+
+using namespace std;
+
+class StrBlob{
+public:
+  typedef vector<string>::size_type size_type;
+  StrBlob();
+  StrBlob(initializer_list<string> il);
+  size_type size() const { return data->size(); }
+  bool empty() const { return data->empty(); }
+
+  // add and remove elements
+  void push_back(const string &t) { data->push_back(t); }
+  void pop_back();
+
+  // element access
+  string& front();
+  string& back();
+
+private:
+  shared_ptr<vector<string>> data;
+  // throws msg if data[i] isn't valid
+  void check(size_type i, const string &msg) const;
+};
+
+int main(){
+  
+
+  return 0;
+}
