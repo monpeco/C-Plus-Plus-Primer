feb3189fd2b2ce195ddb239c2b70f53304083fa0
monpeco <monpeco@gmail.com>
2019-01-15 21:28:00 +0000

    Add StrBlob element access members

--- Chapter12-Dynamic-Memory/12.1-Dynamic-Memory-and-Smart-Pointers/StrBlob.cpp
+++ Chapter12-Dynamic-Memory/12.1-Dynamic-Memory-and-Smart-Pointers/StrBlob.cpp
@@ -31,6 +31,32 @@ StrBlob::StrBlob() : data(make_shared<vector<string>>()) {}
 StrBlob::StrBlob(initializer_list<string> il) : 
              data(make_shared<vector<string>>(il)) {}
 
+// Element Access Members
+void StrBlob::check(size_type i, const string& msg) const{
+  if(i >= data->size())
+    throw out_of_range(msg);
+}
+
+// The `pop_back` and element access members first call `check`.
+// If check succeds, these members forward their work to the 
+// underlying vector operation:
+string& StrBlob::front(){
+  // if the vector is empty, check will throw
+  check(0, "front on empty StrBlob");
+  return data->front();
+}
+
+string& StrBlob::back(){
+  check(0, "back on empty StrBlob");
+  return data->back();
+}
+
+void StrBlob::pop_back(){
+  check(0, "pop_back on empty StrBlob");
+  data->pop_back();
+}
+
+// `front` and `back` members should be overloaded on const
 int main(){
   
 
