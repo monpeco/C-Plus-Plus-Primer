0b0c4ce51235a485af70ae3e62c49599c000834b
monpeco <monpeco@gmail.com>
2019-01-17 21:34:09 +0000

    Add freeing dynamic memory

--- Chapter12-Dynamic-Memory/12.1-Dynamic-Memory-and-Smart-Pointers/memory_exhaustation.cpp
+++ Chapter12-Dynamic-Memory/12.1-Dynamic-Memory-and-Smart-Pointers/memory_exhaustation.cpp
@@ -9,5 +9,17 @@ int main(){
   int *p1 = new int; // if allocation fails, new throws std::bad_alloc
   int  *p2  =  new  (nothrow)  int;  // if allocation fails,  new  returns  a  null pointer
   
+  
+  // Freeing Dynamic Memory
+  delete p2;      // p must point to a dynamically allocated object or be null
+  
+  int i, *pi1 = &i, *pi2 = nullptr;
+  double *pd = new double(33), *pd2 = pd;
+  // delete i;   // error: i is not a pointer
+  // delete pi1; // undefined: pi1 refers to a local
+  delete pd;  // ok
+  // delete pd2; // undefined: the memory pointed to by pd2 was already freed
+  delete pi2; // ok: it is always ok to delete a null pointer
+
   return 0;
 }
