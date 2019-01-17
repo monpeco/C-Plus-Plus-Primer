9492c1e560fed84bbff788fb8d6111e195303857
monpeco <monpeco@gmail.com>
2019-01-17 21:27:54 +0000

    Add memory exhaustation - nothrow object

---
+++ Chapter12-Dynamic-Memory/12.1-Dynamic-Memory-and-Smart-Pointers/memory_exhaustation.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <memory>
+#include <vector>
+
+using namespace std;
+
+int main(){
+  // if allocation fails, new returns a null pointer
+  int *p1 = new int; // if allocation fails, new throws std::bad_alloc
+  int  *p2  =  new  (nothrow)  int;  // if allocation fails,  new  returns  a  null pointer
+  
+  return 0;
+}
