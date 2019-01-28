f841c43335f4ceb9340dd14243700235aeae32ad
monpeco <monpeco@gmail.com>
2019-01-28 21:15:12 +0000

    Add use of lock() in weak_ptr to access object

--- Chapter12-Dynamic-Memory/12.1-Dynamic-Memory-and-Smart-Pointers/weak_ptr.cpp
+++ Chapter12-Dynamic-Memory/12.1-Dynamic-Memory-and-Smart-Pointers/weak_ptr.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <memory>
 
+using std::shared_ptr;
 using std::make_shared;
 using std::weak_ptr;
 
@@ -8,5 +9,17 @@ int main(){
   auto p = make_shared<int>(42);
   weak_ptr<int> wp(p);            //  wp  weakly  shares  with  p;  use  count  in  p  is unchanged
   
+  
+  // Because the object might no longer exist, we cannot use a weak_ptr to access its
+  // object directly. To access that object, we must call lock. The lock function checks
+  // whether the object to which the weak_ptr points still exists. If so, lock returns a
+  // shared_ptr to the shared object. As with any other shared_ptr, we are
+  // guaranteed that the underlying object to which that shared_ptr points continues to
+  // exist at least as long as that shared_ptr exists. 
+  
+  if(shared_ptr<int> np = wp.lock()){    // true if np is not null
+    // inside the if, np shares its object with p
+  }
+
   return 0;
 }
