5963d6f02d5d6f02fa3d35d3c8f8cf2322f1a0e3
monpeco <monpeco@gmail.com>
2019-02-05 21:53:19 +0000

    Add shared_ptr with new

--- Chapter12-Dynamic-Memory/12.1-Dynamic-Memory-and-Smart-Pointers/shared_ptr_with_new.cpp
+++ Chapter12-Dynamic-Memory/12.1-Dynamic-Memory-and-Smart-Pointers/shared_ptr_with_new.cpp
@@ -1,23 +1,15 @@
+#include <iostream>
+#include <vector>
 #include <memory>
 
 using namespace std;
 
 
-shared_ptr<int> clone(int p){
-  return new int(p);  // error: implicit convertion to shared_ptr
-}
-
-shared_ptr<int> clone(int p){
-  return shared_ptr<int>(new int(p));
-}
-
 int main(){
+  shared_ptr<double> p1;              // shared pointer that can point to a double
+  shared_ptr<int> p2(new int(42));    // p2 points to a int with value 42
   
-  shared_ptr<double> p1;                // shared_ptr that can point to a double
-  shared_ptr<int> p2(new int(42));      // p2 points to a int of value 42
-  
-  // shared_ptr<int> p3 = new int(1024);  // error: must use direct initialization
-  shared_ptr<int> p4(new int(42));        // ok: uses direct initialization
-  
+  //shared_ptr<int> p3 = new int(1024);  // error: must use direct initialization
+
   return 0;
 }
