554d14b0cbfd503be31092953260d67554464bc3
monpeco <monpeco@gmail.com>
2019-02-13 21:17:39 +0000

    Add dont use get() to initialize a build-in pointer - memory freed wrongly

--- Chapter12-Dynamic-Memory/12.1-Dynamic-Memory-and-Smart-Pointers/dont_use_get_to_init.cpp
+++ Chapter12-Dynamic-Memory/12.1-Dynamic-Memory-and-Smart-Pointers/dont_use_get_to_init.cpp
@@ -8,6 +8,11 @@ using namespace std;
 int main(){
   shared_ptr<int> p(new int(42));
   int *q = p.get();     // // ok:  but  don't use  q  in  any  way  that  might  delete  its pointer
+  {  // new block
+    //  undefined: two independent shared_ptrs point to the same memory
+    shared_ptr<int>(q);
+  } // block ends, q is destroyed, and the memory to which q points is freed
+  int foo = *p; // undefined; the memory to which p points was freed
   
   return 0;
 }
