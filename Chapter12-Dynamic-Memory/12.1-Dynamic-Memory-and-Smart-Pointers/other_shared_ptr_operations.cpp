d7f3467885db699f4e9adf237b2ff68c5d45d784
monpeco <monpeco@gmail.com>
2019-02-19 19:37:17 +0000

    Add use of reset() with unique() for shared_ptr

--- Chapter12-Dynamic-Memory/12.1-Dynamic-Memory-and-Smart-Pointers/other_shared_ptr_operations.cpp
+++ Chapter12-Dynamic-Memory/12.1-Dynamic-Memory-and-Smart-Pointers/other_shared_ptr_operations.cpp
@@ -10,5 +10,10 @@ int main(){
   //p = new int(42);      // error: cannot assign a pointer to a shared_ptr
   p.reset(new int(42));   // ok: points to a new object
   
+  shared_ptr<string> p2;
+  if (!p2.unique())
+    p2.reset(new string(*p2)); // we aren't alone; allocate a new copy
+  *p2  +=  "new string";       // now that we know we're the only pointer, okay to change this object
+  
   return 0;
 }
