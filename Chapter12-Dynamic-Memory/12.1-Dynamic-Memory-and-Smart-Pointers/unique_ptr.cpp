37b146a4b28e3fa5b4f889f29d62ecbd1cc17348
monpeco <monpeco@gmail.com>
2019-02-21 21:35:06 +0000

    Add no assign for unique_ptr

--- Chapter12-Dynamic-Memory/12.1-Dynamic-Memory-and-Smart-Pointers/unique_ptr.cpp
+++ Chapter12-Dynamic-Memory/12.1-Dynamic-Memory-and-Smart-Pointers/unique_ptr.cpp
@@ -6,6 +6,11 @@ using namespace std;
 int main(){
   unique_ptr<int> p1;
   unique_ptr<int> p2(new int(42));
+  
+  unique_ptr<string> ps1(new string("this is a string"));
+  //unique_ptr<string> ps2(ps1);        ERROR: no copy for unique_ptr
+  unique_ptr<string> ps3;
+  //ps3 = ps1;                          ERROR: no assign for unique_ptr
 
   return 0;
 }
