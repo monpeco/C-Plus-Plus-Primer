5354deb692d2dc8c613f4082b8866b1e7c57154c
monpeco <monpeco@gmail.com>
2019-02-21 21:31:02 +0000

    Add unique_ptr

--- Chapter12-Dynamic-Memory/12.1-Dynamic-Memory-and-Smart-Pointers/unique_ptr.cpp
+++ Chapter12-Dynamic-Memory/12.1-Dynamic-Memory-and-Smart-Pointers/unique_ptr.cpp
@@ -1,26 +1,11 @@
+#include <iostream>
 #include <memory>
 
-using std::unique_ptr;
-using std::string;
+using namespace std;
 
 int main(){
-  
-  std::unique_ptr<double> p1;            // unique_ptr that can point to a double
-  std::unique_ptr<int> p2(new int(42));  // p2 points to a int with value 42
-  
-  unique_ptr<string> p3(new string("Stegosaurus"));
-  // unique_ptr<string> p4(p3);          // error: no copy for unique_ptr
-  unique_ptr<string> p5;
-  //p5 = p3;                             // error: no assign for unique_ptr
-  
-  // transfer ownership from p3 to p4
-  unique_ptr<string> p4(p3.release());   // release make p3 null
-  unique_ptr<string> p6(new string("Trex"));
-  // transfer the ownershipt from p6 to p4
-  p4.reset(p6.release());                // reset deletes the memory to which p4 had pointed
-  
-  p4.release();            // Wrong: p4 wont free the memory and we've lost the pointer
-  auto p7 = p6.release();  // ok, but we must remember to delete p7
-  
+  unique_ptr<int> p1;
+  unique_ptr<int> p2(new int(42));
+
   return 0;
 }
