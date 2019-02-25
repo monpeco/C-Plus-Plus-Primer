e666c00883d6e111a5e2310e8cc22485376e4c1d
monpeco <monpeco@gmail.com>
2019-02-25 20:36:16 +0000

    Add transfer ownership of a pointer (release() and reset())

--- Chapter12-Dynamic-Memory/12.1-Dynamic-Memory-and-Smart-Pointers/unique_ptr.cpp
+++ Chapter12-Dynamic-Memory/12.1-Dynamic-Memory-and-Smart-Pointers/unique_ptr.cpp
@@ -12,5 +12,12 @@ int main(){
   unique_ptr<string> ps3;
   //ps3 = ps1;                          ERROR: no assign for unique_ptr
 
+  // transfers ownership from p1 to p2
+  unique_ptr<string> pu2(ps1.release());  // release makes p1 null
+
+  unique_ptr<string> pu3(new string("Trex"));
+  // transfer ownership from p3 to p2
+  pu2.reset(pu3.release());  // reset deletes the memory to which p2 had pointed
+
   return 0;
 }
