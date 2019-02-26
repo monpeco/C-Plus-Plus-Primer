e3d7d72543667c61033566f4c2aa0951f6b717c5
monpeco <monpeco@gmail.com>
2019-02-26 22:35:25 +0000

    Add release a unique_ptr (the wrong and the dangerous way)

--- Chapter12-Dynamic-Memory/12.1-Dynamic-Memory-and-Smart-Pointers/unique_ptr.cpp
+++ Chapter12-Dynamic-Memory/12.1-Dynamic-Memory-and-Smart-Pointers/unique_ptr.cpp
@@ -19,5 +19,14 @@ int main(){
   // transfer ownership from p3 to p2
   pu2.reset(pu3.release());  // reset deletes the memory to which p2 had pointed
 
+
+  // calling release breaks the connection between a unique_ptr and the object it had been managing.
+  // Often the pointer returned by release is used to initialize or to assign another smart pointer.
+  // In that case, the responsability for managing the memory is simple transferred from ona smart pointer to another.
+  // However, if we do not use another smart pointer to hold the pointer returned from release, our program takes over responsability for freeing that resourse:
+
+  p2.release(); // WRONG: p2 won't free the memory and we've lost the pointer
+  auto p = p2.release();  // ok, but we must remember to delete(p)
+
   return 0;
 }
