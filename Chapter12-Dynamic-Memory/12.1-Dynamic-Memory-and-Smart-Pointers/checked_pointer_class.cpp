0173b9959be1ffea85ba8be95bf98f205df34b81
monpeco <monpeco@gmail.com>
2019-03-05 20:42:29 +0000

    Add StrBlobPtr - checked pointer class

--- Chapter12-Dynamic-Memory/12.1-Dynamic-Memory-and-Smart-Pointers/checked_pointer_class.cpp
+++ Chapter12-Dynamic-Memory/12.1-Dynamic-Memory-and-Smart-Pointers/checked_pointer_class.cpp
@@ -5,34 +5,32 @@
 
 using namespace std;
 
+
 class StrBlobPtr{
 public:
   StrBlobPtr(): curr(0) {}
-  StrBlobPtr(StrBlob &a, size_t sz = 0): wptr(a.data), curr(sz) { }
-  std::string& deref() const;
-  StrBlobPtr& incr();       // prefix version
   
+  StrBlobPtr(StrBlob &a, size_t sz = 0):
+    wptr(a.data), curr(sz) {}
+
+  string& deref() const;
+
+  StrBlobPtr& incr();     // prefix version
 private:
   // check returns a shared_ptr to the vector if the check succeeds
   shared_ptr<vector<string>> check(size_t, const string&) const;
-  // store a weak_ptr, which means the underlying vector might be destroyed
+
+  // store a weak_ptr, which menas the underlying vector might be destroyed
   weak_ptr<vector<string>> wptr;
-  size_t curr;      // current position within the array
+  
+  std::size_t curr;    // current position within the array
 };
 
-shared_ptr<vector<string>> StrBlobPtr::check(size_t i, const string &msg) const{
-  auto ret = wptr.lock();    // is the vector still around?
-  if(!ret){
-    throw runtime_error("Unbound StrBlobPtr");
-  }
-  if(i >= ret->size()){
-    throw out_of_range(msg);
-  }
-  return ret;
-}
-
 int main(){
-
-
-  return 0;
+  auto p = make_shared<int>(42);
+  weak_ptr<int> wp(p);    // wp weakly shares with p; use count in p is unchanged
+  
+  if (shared_ptr<int> np = wp.lock()){    // true if np is not null
+    // inside the if, np shares its object with p
+  }
 }
