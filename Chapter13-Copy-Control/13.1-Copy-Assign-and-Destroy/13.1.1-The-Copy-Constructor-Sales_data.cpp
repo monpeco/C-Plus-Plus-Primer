5195d297bfad2227d9a76f17f68b6e4f32cbef70
monpeco <monpeco@gmail.com>
2019-04-06 12:16:27 +0000

    Add Sales_data copy constructor implementation

--- Chapter13-Copy-Control/13.1-Copy-Assign-and-Destroy/13.1.1-The-Copy-Constructor-Sales_data.cpp
+++ Chapter13-Copy-Control/13.1-Copy-Assign-and-Destroy/13.1.1-The-Copy-Constructor-Sales_data.cpp
@@ -5,12 +5,18 @@ class Sales_data{
     // other members and constructor as before
     // declaration equivalent to the synthesized copy constructor
     Sales_data(){ std::cout << "default constuctor \n"; };
-    Sales_data(const Sales_data&){ std::cout << "copy constuctor \n"; };
+    Sales_data(const Sales_data&);
   private:
     std::string bookNo;
     int units_sold = 0;
     double revenue = 0.0;
 };
+
+Sales_data::Sales_data(const Sales_data& orig) :
+  bookNo(orig.bookNo),             // uses the string copy constructor
+  units_sold(orig.units_sold),     // copies orig.units_sold
+  revenue(orig.revenue)            // copies orig.revenue
+  { std::cout << "copy constuctor \n";  }
 
 int main(){
     Sales_data f1;
