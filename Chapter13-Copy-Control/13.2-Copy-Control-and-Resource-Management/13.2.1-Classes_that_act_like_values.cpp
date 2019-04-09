a13fa7df40a4d26d69937359e8fea8b350c758b3
monpeco <monpeco@gmail.com>
2019-04-09 18:29:02 +0000

    Add classes that act like value

--- Chapter13-Copy-Control/13.2-Copy-Control-and-Resource-Management/13.2.1-Classes_that_act_like_values.cpp
+++ Chapter13-Copy-Control/13.2-Copy-Control-and-Resource-Management/13.2.1-Classes_that_act_like_values.cpp
@@ -5,6 +5,13 @@ public:
   HasPtr(const std::string &s = std::string()) :
     ps(new std::string(s)), i(0) {}
     
+    //each HasPtr has its own copy of the string to which ps points
+    HasPtr(const HasPtr &p) :
+      ps(new std::string(*p.ps)), i(p.i) {}
+      
+    HasPtr& operator=(const HasPtr &);
+    ~HasPtr() { delete ps; }
+    
 private:
   std::string *ps;
   int i;
