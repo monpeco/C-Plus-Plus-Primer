dbe45e303b820258fec4168368519e08f71a02d5
monpeco <monpeco@gmail.com>
2019-04-16 20:09:36 +0000

    Add classes that ack like pointer

---
+++ Chapter13-Copy-Control/13.2-Copy-Control-and-Resource-Management/13.2.2-Classes-That-Act-Like-Pointers.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+class HasPtr {
+public:
+    // constructor allocates a new string and a new counter, which it sets to 1
+    HasPtr(const std::string &s = std::string()):
+            ps(new  std::string(s)),  i(0), use(new std::size_t(1)) {}
+    // copy constructor copies all three data members and increments the counter
+    HasPtr(const HasPtr &p):
+        ps(p.ps), i(p.i), use(p.use) { ++*use; }
+    HasPtr& operator=(const HasPtr&);
+    ~HasPtr();
+private:
+    std::string *ps;
+    int    i;
+    std::size_t *use;   // member to keep track of how many objects share *ps
+};
+
+
+
+int main(){
+    
+    
+    
+    return 0;
+}
